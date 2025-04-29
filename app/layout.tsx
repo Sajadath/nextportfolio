90a489183672110c9c1dda8171127e3274b060cf
sajad <sajadath1@gmail.com>
2025-04-29 23:00:50 +0330

    og possible fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -28,8 +28,6 @@ export const metadata: Metadata = {
     images: [
       {
         url: "/og.jpg",
-        width: 1200,
-        height: 630,
         alt: "Sajad Atharyan Portfolio",
       },
     ],
